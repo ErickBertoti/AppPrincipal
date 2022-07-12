fe808974bc375d02e4465c1d252b971f14e74b67
Erick Hoffmann Bertoti <95359762+ErickBertoti@users.noreply.github.com>
2022-07-12 12:20:29 -0300

    Add files via upload

---
+++ app/src/main/java/br/com/telaserick/LoginActivity.java
@@ -0,0 +1,39 @@
+package br.com.telaserick;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        final Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CamisasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+    }
+
+}
